6a27536099a56a600855c5189c4d89693e6e543a
sghus <sghuste@gmail.com>
2020-02-09 19:41:27 -0800

    Updated Company Class

--- src/com/company/Company.java
+++ src/com/company/Company.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.List;
+
 public class Company {
     private String name;
     private String address;
+    private Owner owner;
+    private List<Employee> employees;
 
     public void hireNewEmployee(Employee e){
 
