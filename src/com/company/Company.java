41c79976f7d4b58bce7d0f84b37cf1206d6f9548
EpsilonOne <G00s3d25236>
2020-02-10 14:28:20 -0800

    Set to default

--- src/com/company/Company.java
+++ src/com/company/Company.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class Company {
     private String name;
     private String address;
-    private Owner owner;
-    private List<Employee> employees;
+
+
 
     public void hireNewEmployee(Employee e){
 
